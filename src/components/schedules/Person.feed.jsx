e45afc2c1d4aa10158b376e9d9bad28d6130f167
wellington.pinho@ahgora.com.br <wellington.pinho@ahgora.com.br>
2021-11-14 11:35:45 -0300

    new 1.0.6

--- src/components/schedules/Person.feed.jsx
+++ src/components/schedules/Person.feed.jsx
@@ -5,7 +5,7 @@ const Card = () => {
             <img src="https://img.favpng.com/20/2/6/circle-computer-icons-button-person-icon-png-favpng-6BzqzV9DFYcskZFx5y6vYTcm2.jpg" class="card-img-top" alt="..." />
             <div class="card-body">
                 <h5 class="card-title">Feedback recebido</h5>
-                <p class="card-text">Parabéns a equipe Ahfeed. Apredemos muito, compartilhamos conhecimentos. Isso não tem preço. E o futuro, fazemos juntos e Ahgora!</p>
+                <p class="card-text">Parabéns a equipe Ahfeed. Aprendemos muito, compartilhamos conhecimentos. Isso não tem preço. E o futuro, fazemos juntos e Ahgora!</p>
                 <a href="/schedules" className="btn btn-primary">Voltar</a>
             </div>
         </div>
