642151135c8be59fa97b1918ac42a119913567fe
wellington.pinho@ahgora.com.br <wellington.pinho@ahgora.com.br>
2021-11-14 10:36:24 -0300

    new 1.0.4

--- src/components/schedules/BottomBar.jsx
+++ src/components/schedules/BottomBar.jsx
@@ -2,7 +2,11 @@ const BottomBar = () => {
     return (
         <div class="fixed-bottom mt-3 ml-3">
             <div className="bg-white d-flex flex-row-reverse p-5 pt-2 pb-2">
-
+            <div className="mb-1">
+                    <a href="/rating" class="btn btn-warning m-1 mt-1">
+                        Rating
+                    </a>
+                </div>
                 <div className="mb-1">
                     <a href="/scheduling" class="btn btn-success m-1 mt-1">
                         Novo
